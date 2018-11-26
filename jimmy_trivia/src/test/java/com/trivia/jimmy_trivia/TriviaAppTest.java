068c8ce9321d92d136b853a7da1a2839cc09ca41
WHQ_NT_DOMAIN\JG026240 <jimmy.gentile@cerner.com>
2018-11-25 20:00:03 -0600

    First Commit

---
+++ jimmy_trivia/src/test/java/com/trivia/jimmy_trivia/TriviaAppTest.java
@@ -0,0 +1,14 @@
+package com.trivia.jimmy_trivia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TriviaAppTest {
+
+	@Test
+	public void test() {
+		assertTrue( true );
+	}
+
+}
